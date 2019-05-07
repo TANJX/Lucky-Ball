d647757408ef7a9ba71f7f0f125775161814abc6
Jianxuan Tan <jianxuat@usc.edu>
2019-05-07 10:46:39 -0700

    launcher and its position

--- sketch.js
+++ sketch.js
@@ -15,6 +15,7 @@ function setup() {
   img_rod_left = loadImage('assets/rod_left.png');
   img_rod_right = loadImage('assets/rod_right.png');
   img_logo = loadImage('assets/logo.png');
+  img_launcher = loadImage('assets/launcher.png');
 
   textFont(font);
   textSize(52);
@@ -32,7 +33,7 @@ let img_button_up;
 let img_rod_center;
 let img_rod_left;
 let img_rod_right;
-
+let img_launcher;
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
@@ -49,13 +50,14 @@ function draw() {
   noStroke();
   rect(0, 0, width, height);
 
-  fill('#818181');
+  fill('#505c81');
   noStroke();
-  rect(0, height * 0.85, width, height * 0.2);
+  rect(0, height - 130, width, 130);
 
-  fill('#afafaf');
+  image(img_launcher, (width - 180) * launcher_location / 100, 0, 180, 180);
+  fill('rgba(100,100,100,0.28)');
   noStroke();
-  rect(0, 0, width, height * 0.1);
+  rect(0, 0, width, 90);
 
 
   let img_button;
@@ -63,10 +65,18 @@ function draw() {
 
   if (keyIsDown(LEFT_ARROW) && !keyIsDown(RIGHT_ARROW)) {
     img_rod = img_rod_left;
+    launcher_location -= 1;
   } else if (keyIsDown(RIGHT_ARROW) && !keyIsDown(LEFT_ARROW)) {
     img_rod = img_rod_right;
+    launcher_location += 1;
   } else {
     img_rod = img_rod_center;
+  }
+
+  if (launcher_location < 0) {
+    launcher_location = 0;
+  } else if (launcher_location > 100) {
+    launcher_location = 100;
   }
 
   if (keyIsDown(88) || keyIsDown(32)) {
@@ -75,11 +85,14 @@ function draw() {
     img_button = img_button_up;
   }
 
-  image(img_button, width - 450, height * 0.85 - 128, 256, 256);
-  image(img_rod, width - 256, height * 0.85 - 128, 256, 256);
+  image(img_button, width - 450, height - 130 - 140, 256, 256);
+  image(img_rod, width - 256, height - 130 - 140, 256, 256);
 
   image(img_coin, width - 256, 0, 96, 96);
   fill('#000');
   text('123', width - 256 + 105, 30);
   image(img_logo, 24, 24, 7 * 8 * 6, 8 * 6);
 }
+
+let coins = 5;
+let launcher_location = 50;
